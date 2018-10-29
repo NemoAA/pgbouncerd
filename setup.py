65a6eb95052fa174ab45041e03ded926162c4c77
nemo <dbyzaa@163.com>
2018-10-29 15:55:08 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+import sys
+from setuptools import setup
+
+data_files = None
+
+if sys.version_info < (2, 6):
+    raise SystemExit('ERROR: pgbouncerd need at least python 2.6 to work.')
+
+setup(
+    name='pgbouncerd',
+    version='0.1',
+    author='Nemo deng',
+    author_email='dbyzaa@163.com',
+    scripts=['pgbouncerd'],
+    url='https://github.com/NemoAA/pg_script/tree/master/pgbouncerd',
+    license='LICENSE.txt',
+    description='Command line tool for PgBouncer server daemon.',
+    install_requires=[
+        "psutil >= 0.4.1",
+        "setproctitle >= 1.1.10",
+    ],
+    data_files=data_files,
+)
